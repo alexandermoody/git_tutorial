915ec0256f4daf2fbfdf77c38611574ad8e1288b
amoody <amoody@hmc.edu>
2019-02-03 16:23:22 -0800

    Fix code and remove unnecessary file

--- impulse_response.m
+++ impulse_response.m
@@ -8,8 +8,9 @@ function [ h,t ] = impulse_response( x, y, fs )
 
 [X,f] = fdomain(x,fs);
 [Y,f] = fdomain(y,fs);
-H = X./Y;   % the calculated frequency response
+H = Y./X;   % the calculated frequency response %Alex Moody corrected expression to be output over input
 [h,t] = tdomain(H,fs);
+plot(t,h); %plot impulse response in time domain
 
 end
 
